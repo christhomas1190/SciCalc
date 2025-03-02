b739497b7120546485bb4c40937ae3ed7097271a
isiah-a <isiaha478@gmail.com>
2025-03-02 12:02:33 -0500

    Added comments for math in Science file

--- src/main/java/com/zipcodewilmington/scientificcalculator/Science.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Science.java
@@ -5,15 +5,19 @@ import java.lang.Math;
 
 public class Science {
 
+
     public static double square(double num){
         return num*num;
     }
+    //Returns the value of the first argument raised to the power of the second argument
     public static double pow(double a, double b){
         return Math.pow(a, b);
     }
+    //Returns the correctly rounded positive square root of a double value
     public static double squareRt(double num) {
         return Math.sqrt(num);
     }
+
     public static double inverse(double num) {
         return 1 / num;
     }
@@ -27,22 +31,28 @@ public class Science {
     public static double switchSign(double num) {
         return Math.signum(num);
     }
+    //Returns the trigonometric sine of an angle
     public static double sin(double num) {
         return Math.sin(num);
     }
+    //Returns the trigonometric cosine of an angle
     public static double cos(double num) {
         return Math.cos(num);
     }
+    //Returns the trigonometric tangent of an angle
     public static double tan(double num) {
         return Math.tan(num);
     }
+    //Returns the arc cosine of a value; the returned angle is in the range 0.0 through pi.
     public static double inverseCos(double num) {
         return Math.acos(num);
     }
+    //Returns the arc tangent of a value; the returned angle is in the range -pi/ 2 through pi/ 2
     public double inverseTan(double num) {
         return Math.atan(num);
     }
-        //using BigInteger import
+        //using BigInteger import...returns the product of all positive integers less than or equal
+        // to a given positive integer
     public static BigInteger factorial(int num) {
         BigInteger b = new BigInteger("1");
         for(int i = 2; i <= num; i++){
@@ -74,32 +84,34 @@ public class Science {
         }
     }
 
-    //Degrees
+    //Degrees: Converts an angle measured in radians to an
+    // approximately equivalent angle measured in degrees.
     public static double degrees(double num) {
         return Math.toDegrees(num);
     }
 
-    //Radians
+    //Radians: Converts an angle measured in degrees to an
+    // approximately equivalent angle measured in radians.
     public static double radians(double num) {
             return Math.toRadians(num);
     }
 
-    //Log
+    //Log: Returns the natural logarithm (base e) of a double value.
     public static double log(double num) {
         return Math.log(num);
 
     }
 
-    //Inverse Log
+    //Inverse Log: Returns the base 10 logarithm of a double value.
     public static double log10(double num) {
         return Math.log10(num);
     }
 
-    //Natural logarithm
+    //Natural logarithm: Returns the natural logarithm of the sum of the argument and 1.
     public static double log1p(double num) {
         return Math.log1p(num);
     }
-    //Inverse Natural Log
+    //Inverse Natural Log: Returns Euler's number e raised to the power of a double value.
     public static double inverseNL(double num) {
         return Math.exp(num);
     }
