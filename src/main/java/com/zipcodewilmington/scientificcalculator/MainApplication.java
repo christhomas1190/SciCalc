bf690f1f1fb4ca9bb82bcb476718ccd36b5e5579
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-02 12:57:44 -0500

    Almost there???

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,6 +1,5 @@
 package com.zipcodewilmington.scientificcalculator;
 import java.util.Scanner;
-import java.math.BigInteger;
 /**
  * Created by leon on 2/9/18.
  */
@@ -18,37 +17,134 @@ public class MainApplication {
             System.out.println("3: Multiplication");
             System.out.println("4: Division");
             System.out.println("5: Square");
-            System.out.println("6: Square root");
-            System.out.println("7: Sine");
-            System.out.println("8: Cosine");
-            System.out.println("8: Tangent");
-            System.out.println("9: Inverse Sine");
-            System.out.println("10: Inverse Cosine");
-            System.out.println("13: Quit application");
+            System.out.println("6: Cube/Exponent");
+            System.out.println("7: Square root");
+            System.out.println("8: Sine");
+            System.out.println("9: Cosine");
+            System.out.println("10: Tangent");
+            System.out.println("11: Inverse Sine");
+            System.out.println("12: Inverse Cosine");
+            System.out.println("13: Inverse Tangent");
+            System.out.println("14: Inverse");
+            System.out.println("15: Switch sign");
+            System.out.println("16: Factorial");
+            System.out.println("17: Degrees");
+            System.out.println("18: Radians");
+            System.out.println("19: log");
+            System.out.println("20: log10");
+            System.out.println("21: log1p");
+            System.out.println("22: Inverse Natural Log");
+            System.out.println("23:Store Memory");
+            System.out.println("24:Reset Memory");
+            System.out.println("25:Recall Memory");
+            System.out.println("26: Quit application");
                         //SwitchDisplayMode() = create multiple display menus instead of one large one?
 
             int choice = scanner.nextInt(); //Close scanner when all inputs are built
 
-            if(choice == 13){
+            if(choice == 26){
                 break;
             }
+
             double num1=0;
             double num2=0;
 
-            if(choice >=1 && choice <= 4){
-                num1= Console.getDoubleInput("Enter first number here :");
-                num2= Console.getDoubleInput("Enter second number here :");
+            if(choice >=1 && choice <= 4 || choice ==6 || choice ==25|| choice ==23){
+                System.out.println("Enter first number: ");
+                num1=scanner.nextDouble();
+                System.out.println("Enter second number: ");
+                num2=scanner.nextDouble();
+            }else if ( choice ==5  || choice ==7 || (choice>8 && choice <=25)){
+                num1= Console.getDoubleInput("Enter number here :");
             }
+
+
             Science science = new Science();
             switch(choice){
                 case 1:
                     System.out.println("Result:" + science.addition(num1, num2));
                     break;
-            }
 
+                case 2:
+                    System.out.println("Result:" + science.subtraction(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Result:" + science.multiplication(num1, num2));
+                    break;
+                case 4:
+                    System.out.println("Result:" + science.division(num1, num2));
+                    break;
+                case 5:
+                    System.out.println("Result:" + science.square(num1));
+                    break;
+                case 6:
+                    System.out.println("Result:" + science.pow(num1, num2));
+                    break;
+                case 7:
+                    System.out.println("Result:" + science.squareRt(num1));
+                    break;
+                case 8:
+                    System.out.println("Result:" + science.sin(num1));
+                    break;
+                case 9:
+                    System.out.println("Result:" + science.cos(num1));
+                    break;
+                case 10:
+                    System.out.println("Result:" + science.tan(num1));
+                    break;
+                case 11:
+                    System.out.println("Result:" + science.inverseSin(num1));
+                    break;
+
+                case 12:
+                System.out.println("Result:" + science.inverseCos(num1));
+                    break;
+                case 13:
+                    System.out.println("Result:" + science.inverseTan(num1));
+                    break;
+
+                case 14:
+                    System.out.println("Result:" + science.inverse(num1));
+                    break;
+                case 15:
+                    System.out.println("Result:" + science.switchSign(num1));
+                    break;
+//                case 16:
+//                    System.out.println("Result:" + science.factorial(num1);
+//                    break;
+                case 17:
+                    System.out.println("Result:" + science.degrees(num1));
+                    break;
+                case 18:
+                    System.out.println("Result:" + science.radians(num1));
+                    break;
+                case 19:
+                    System.out.println("Result:" + science.log(num1));
+                    break;
+                case 20:
+                    System.out.println("Result:" + science.log10(num1));
+                    break;
+                case 21:
+                    System.out.println("Result:" + science.log1p(num1));
+                    break;
+                case 22:
+                    System.out.println("Result:" + science.inverseNL(num1));
+                    break;
+                case 23:
+                    System.out.println("Result:" + science.storeMemory(num1, num2));
+                    break;
+                case 24:
+                    System.out.println("Result:" + science.resetMemory(num1));
+                    break;
+                case 25:
+                    System.out.println("Result:" + science.recallMemory(num1, num2));
+                    break;
+
+            }
         }
     }
 }
+
 //System.out.print(memory);
 //break
 
--- src/main/java/com/zipcodewilmington/scientificcalculator/Science.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Science.java
@@ -4,53 +4,6 @@ import java.lang.Math;
 
 
 public class Science {
-
-    public static double square(double num){
-        return num*num;
-    }
-    public static double pow(double a, double b){
-        return Math.pow(a, b);
-    }
-    public static double squareRt(double num) {
-        return Math.sqrt(num);
-    }
-    public static double inverse(double num) {
-        return 1 / num;
-    }
-
-    //If absolute value of number is greater than 1 the result will be NaN
-    public static double inverseSine(double num) {
-        return Math.asin(num);
-    }
-
-    //Returns sign of number 1 for positive, -1 for negative and 0 for zero
-    public static double switchSign(double num) {
-        return Math.signum(num);
-    }
-    public static double sin(double num) {
-        return Math.sin(num);
-    }
-    public static double cos(double num) {
-        return Math.cos(num);
-    }
-    public static double tan(double num) {
-        return Math.tan(num);
-    }
-    public static double inverseCos(double num) {
-        return Math.acos(num);
-    }
-    public double inverseTan(double num) {
-        return Math.atan(num);
-    }
-        //using BigInteger import
-    public static BigInteger factorial(int num) {
-        BigInteger b = new BigInteger("1");
-        for(int i = 2; i <= num; i++){
-            b = b.multiply(BigInteger.valueOf(i));
-        }
-        return b;
-    }
-
     //Addition
     public static double addition(double a, double b) {
         return a + b;
@@ -73,13 +26,64 @@ public class Science {
             return a / b;
         }
     }
+    //Squares given number
+    public static double square(double num){
+        return num*num;
+    }
+    // can be used to cube the number, or for exponents
+    public static double pow(double a, double b){
+        return Math.pow(a, b);
+    }
+    //Used to find square root of a number
+    public static double squareRt(double num) {
+        return Math.sqrt(num);
+    }
+    public static double sin(double num) {
+        return Math.sin(num);
+    }
+    public static double cos(double num) {
+        return Math.cos(num);
+    }
+    public static double tan(double num) {
+        return Math.tan(num);
+    }
+    public static double inverseSin(double num) {
+        return Math.asin(num);
+    }
+    public static double inverseCos(double num) {
+        return Math.acos(num);
+    }
+    public double inverseTan(double num) {
+        return Math.atan(num);
+    }
+    //If absolute value of number is greater than 1 the result will be NaN
 
-    //Degrees
+    //
+    public static double inverse(double num) {
+        return 1 / num;
+    }
+
+    //Returns sign of number 1 for positive, -1 for negative and 0 for zero
+    public static double switchSign(double num) {
+        return Math.signum(num);
+    }
+
+
+        //using BigInteger import
+//    public static BigInteger factorial(int num) {
+//        BigInteger b = new BigInteger("1");
+//        for(int i = 2; i <= num; i++){
+//            b = b.multiply(BigInteger.valueOf(i));
+//        }
+//        return b.doubleValue();
+//    }
+
+    //Converts radians to degrees
     public static double degrees(double num) {
         return Math.toDegrees(num);
     }
 
-    //Radians
+    //Converts degrees to radians
     public static double radians(double num) {
             return Math.toRadians(num);
     }
