2568ab23239b8f33c6d968fa56c2c89201ae8d4c
isiah-a <isiaha478@gmail.com>
2025-03-05 13:32:59 -0500

    added tests for sin, pow, inverse, inverse sine, switch sign, cos , tan, inverse cos, inverse tan, add, subtract, multiply, division, log 10, log1p.

--- src/test/java/com/zipcodewilmington/scientific_calculator/ScienceTest.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/ScienceTest.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.scientific_calculator;
 import com.zipcodewilmington.scientificcalculator.Science;
 import org.junit.Test;
 
+import static java.lang.Double.NaN;
 import static org.junit.Assert.assertEquals;
 
 public class ScienceTest {
@@ -68,10 +69,113 @@ public class ScienceTest {
     public void testadd(){
         Science science1 = new Science();
         double actual = science1.addition(10, 2);
-        assertEquals(12, 12, 0.001);
+        assertEquals(12, actual , 0.001);
     }
+
+    @Test
+    public void testinverseCos(){
+        Science science1 = new Science();
+        double actual = science1.inverseCos(2);
+        assertEquals(NaN, actual, 0.0001);
+    }
+    @Test
+    public void testinverse(){
+        Science science1 = new Science();
+        double actual = science1.inverse(10);
+        assertEquals(0.1, actual , 0.000001);
+    }
+
+    @Test
+    public void testinverseSine(){
+        Science science1 = new Science();
+        double actual = science1.inverseSine(10);
+        assertEquals(NaN, actual , 0.000001);
+    }
+
+    @Test
+    public void testswitchSign(){
+        Science science1 = new Science();
+        double actual = science1.switchSign(10);
+        assertEquals(1, actual , 0.000001);
+    }
+
+    @Test
+    public void testcos(){
+        Science science1 = new Science();
+        double actual = science1.cos(10);
+        assertEquals(-0.8390715290764524, actual , 0.000001);
+    }
+
+    @Test
+    public void testtan(){
+        Science science1 = new Science();
+        double actual = science1.tan(10);
+        assertEquals(0.6483608274590866, actual , 0.000001);
+    }
+
+    @Test
+    public void testinversetan(){
+        Science science1 = new Science();
+        double actual = science1.inverseTan(3);
+        assertEquals(1.2490457723982544, actual , 0.000001);
+    }
+
+    @Test
+    public void testsub(){
+        Science science1 = new Science();
+        double actual = science1.subtraction(3, 10);
+        assertEquals(-7, actual , 0.000001);
+    }
+
+    @Test
+    public void testmulti(){
+        Science science1 = new Science();
+        double actual = science1.multiplication(-3, 10);
+        assertEquals(-30, actual , 0.000001);
+    }
+
+    @Test
+    public void testdivision(){ //test will throw
+        // ArithmeticException("Can't divide by 0") if dividing by zero
+        Science science1 = new Science();
+        double actual = science1.division(-3, 10);
+        assertEquals(-0.3, actual , 0.000001);
+    }
+
+    @Test
+    public void testdegrees(){
+        Science science1 = new Science();
+        double actual = science1.degrees(10);
+        assertEquals(572.9577951308232, actual , 0.000001);
+    }
+
+    @Test
+    public void testlog10(){
+        Science science1 = new Science();
+        double actual = science1.log10(5);
+        assertEquals(0.6989700043360189, actual , 0.000001);
+    }
+
+    @Test
+    public void testlog1p(){
+        Science science1 = new Science();
+        double actual = science1.log1p(5);
+        assertEquals(1.791759469228055, actual , 0.000001);
+    }
+
+    @Test
+    public void testinverseNL(){
+        Science science1 = new Science();
+        double actual = science1.inverseNL(5);
+        assertEquals(148.4131591025766, actual , 0.000001);
+    }
+
+
+
+
+
 }
 
-//test
+
 
 
